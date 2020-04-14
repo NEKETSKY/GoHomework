f5ad39862c010215d59dca65117a734aafeb0ec7
NEKETSKY <nikitaverbovikov@gmail.com>
2020-04-14 11:17:58 +0300

    fix remarks

--- homework2/square.go
+++ homework2/square.go
@@ -2,24 +2,31 @@ package main
 
 import "fmt"
 
+//type contains 2 variable point coordinates
 type Point struct {
 	x, y int
 }
 
+//type contains the coordinate of the starting point of the square and the length of its side
 type Square struct {
 	start Point
 	a     uint
+	//"start" variable contains the starting point of type point, "a" variable contains the length of the side of the square
 }
 
-func (z Square) End() Point {
-	return Point{z.start.x + int(z.a), z.start.y - int(z.a)}
+//returns the end point of the square
+func (s Square) End() Point {
+	return Point{s.start.x + int(s.a), s.start.y - int(s.a)}
 }
 
-func (z Square) Perimeter() uint {
-	return z.a * 4
+//returns the perimeter of a square
+func (s Square) Perimeter() uint {
+	return s.a * 4
 }
-func (z Square) Area() uint {
-	return z.a * z.a
+
+//returns the square area
+func (s Square) Area() uint {
+	return s.a * s.a
 }
 
 func main() {
